325a1a1d21c07713fbfe22eb1d2de9026a4ca72e
Neon <77470404+NeonGamer@users.noreply.github.com>
2022-08-21 01:38:55 -0500

    Added Source Code

---
+++ LSystemGenerator/LSystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystemGenerator
+{
+    public class LSystem
+    {
+        public static string Generate(string axiom, int iterations, Dictionary<string, string> rules)
+        {
+            var sb = new StringBuilder();
+            sb.Append(axiom);
+            for (int i = 0; i < iterations; i++)
+            {
+                sb = ApplyRules(sb, rules);
+            }
+            return sb.ToString();
+        }
+        public static StringBuilder ApplyRules(StringBuilder sb, Dictionary<string, string> rules)
+        {
+            int oriLength = sb.Length;
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb.Append(rules[sb[i].ToString()]);
+            }
+            sb.Remove(0, oriLength);
+            return sb;
+        }
+    }
+}
